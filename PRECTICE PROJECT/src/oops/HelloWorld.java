2824c880af930486002eb5c2a81d980e00381c02
krishnakosamia <krishnadodiya1715@gmail.com>
2023-02-05 13:34:54 -0500

    commit

---
+++ PRECTICE PROJECT/src/oops/HelloWorld.java
@@ -0,0 +1,13 @@
+package oops;
+
+public class HelloWorld {
+	//method
+	
+	public static void main(String []args) {
+		
+		System.out.println("This is my first program");
+		 
+		//statments
+	}
+	
+	}
